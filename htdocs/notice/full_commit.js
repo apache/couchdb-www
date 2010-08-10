410a0e84c53e0b08d64c1f0926d002b0f42a12ef
Paul Joseph Davis <davisp@apache.org>
2010-08-10 17:42:33 +0000

    Fixed the 1.0.1.txt bookmarklet.
    Apparently there was Python string interpolation. This requires 
    literal percent signs to be doubled up. Ie, %% -> %. 
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/site@984144 13f79535-47bb-0310-9956-ffa450edef68

--- htdocs/notice/full_commit.js
+++ htdocs/notice/full_commit.js
@@ -21,5 +21,16 @@ $.couch.allDbs({
   }
 });
 
-// the href for the bookmarklet, needs to be copied directly to the html, as build.py chokes on it
+// Condensed to a bookmarklet. When inserting into the 1.0.1.txt
+// template you have to double the % escapes so Python doesn't
+// choke. In vim, you can run the following command on the line:
+//
+//     :s/%/%%/g
+//
+// I've included the double-percents version below.
+//
 javascript:%24.couch.allDbs%28%7Bsuccess%3Afunction%28dbs%29%7Bfunction%20commitDbs%28list%29%7Bvar%20db%3Dlist.pop%28%29%3B%24.ajax%28%7Btype%3A%22POST%22%2Curl%3A%22%2F%22%2BencodeURIComponent%28db%29%2B%22%2F_ensure_full_commit%22%2CcontentType%3A%22application%2Fjson%22%2CdataType%3A%22json%22%2Ccomplete%3Afunction%28r%29%7B%24%28%22%23content%22%29.prepend%28%27%3Cul%20id%3D%22commit_all%22%3E%3C%2Ful%3E%27%29%3Bif%28r.status%3D%3D201%29%7B%24%28%22%23commit_all%22%29.append%28%27%3Cli%3Ecommitted%3A%20%27%2Bdb%2B%27%3C%2Fli%3E%27%29%3B%7Delse%7B%24%28%22%23commit_all%22%29.append%28%27%3Cli%20style%3D%22color%3Ared%3B%22%3Eerror%3A%20%27%2Bdb%2B%27%3C%2Fli%3E%27%29%3B%7Dif%28list.length%3E0%29%7BcommitDbs%28list%29%3B%7D%7D%7D%29%3B%7DcommitDbs%28dbs%29%3B%7D%7D%29%3B
+
+// The version for 1.0.1.txt
+
+javascript:%%24.couch.allDbs%%28%%7Bsuccess%%3Afunction%%28dbs%%29%%7Bfunction%%20commitDbs%%28list%%29%%7Bvar%%20db%%3Dlist.pop%%28%%29%%3B%%24.ajax%%28%%7Btype%%3A%%22POST%%22%%2Curl%%3A%%22%%2F%%22%%2BencodeURIComponent%%28db%%29%%2B%%22%%2F_ensure_full_commit%%22%%2CcontentType%%3A%%22application%%2Fjson%%22%%2CdataType%%3A%%22json%%22%%2Ccomplete%%3Afunction%%28r%%29%%7B%%24%%28%%22%%23content%%22%%29.prepend%%28%%27%%3Cul%%20id%%3D%%22commit_all%%22%%3E%%3C%%2Ful%%3E%%27%%29%%3Bif%%28r.status%%3D%%3D201%%29%%7B%%24%%28%%22%%23commit_all%%22%%29.append%%28%%27%%3Cli%%3Ecommitted%%3A%%20%%27%%2Bdb%%2B%%27%%3C%%2Fli%%3E%%27%%29%%3B%%7Delse%%7B%%24%%28%%22%%23commit_all%%22%%29.append%%28%%27%%3Cli%%20style%%3D%%22color%%3Ared%%3B%%22%%3Eerror%%3A%%20%%27%%2Bdb%%2B%%27%%3C%%2Fli%%3E%%27%%29%%3B%%7Dif%%28list.length%%3E0%%29%%7BcommitDbs%%28list%%29%%3B%%7D%%7D%%7D%%29%%3B%%7DcommitDbs%%28dbs%%29%%3B%%7D%%7D%%29%%3B
